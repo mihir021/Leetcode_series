d483e2cde4bb361aa0bda1b7d9d3d51ed0af75f4
mihir021 <rathodmihir1113@gmail.com>
2025-03-17 17:56:03 +0530

    leet 69

---
+++ LeetGit69.java
@@ -0,0 +1,11 @@
+public class LeetGit69 {
+    public static void main(String[] args) {
+        Solution69 obj = new Solution69();
+        obj.mySqrt(3);
+    }
+}
+class Solution69 {
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x); 
+    }
+}
